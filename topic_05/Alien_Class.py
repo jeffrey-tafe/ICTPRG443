af5f38cb4aee9fe386a84aadd5ad470dca2adf6a
jeffrey-tafe <141377378+jeffrey-tafe@users.noreply.github.com>
2023-09-20 15:48:03 +0930

    minor clean up indents

--- topic_05/Alien_Class.py
+++ topic_05/Alien_Class.py
@@ -1,31 +1,32 @@
 class Alien:
-  __valid_alien_types = ["Martian", "Venusian", "Plutonian", "Neptunian"]
 
-  def __init__(self, strength=0, lives=0, alien_type = "Alien Type Not Set"):
-    self.__strength = strength
-    self.__lives = lives
-    self.set_alien_type(alien_type)
+    __valid_alien_types = ["Martian", "Venusian", "Plutonian", "Neptunian"]
 
-  def set_strength(self, strength):
-    self.__strength = strength
+    def __init__(self, strength = 0, lives = 0, alien_type = "Alien Type Not Set"):
+        self.__strength = strength
+        self.__lives = lives
+        self.set_alien_type(alien_type)
 
-  def set_lives(self, lives):
-    self.__lives = lives
+    def set_strength(self, strength):
+        self.__strength = strength
 
-  def set_alien_type(self, new_alien_type):
-    if new_alien_type in Alien.__valid_alien_types:
-      self.__alien_type = new_alien_type.strip()
-    else:
-      self.__alien_type = "Alien Type Not Set"
+    def set_lives(self, lives):
+        self.__lives = lives
 
-  def get_alien_type(self):
-    return self.__alien_type
+    def set_alien_type(self, new_alien_type):
+        if new_alien_type in Alien.__valid_alien_types:
+            self.__alien_type = new_alien_type.strip()
+        else:
+            self.__alien_type = "Alien Type Not Set"
 
-  def get_strength(self):
-    return self.__strength
+    def get_alien_type(self):
+        return self.__alien_type
 
-  def get_lives(self):
-    return self.__lives
+    def get_strength(self):
+        return self.__strength
 
-  def __str__(self):
-    return f"\tStrength: {self.get_strength()}\n\tLives: {self.get_lives()}"
+    def get_lives(self):
+        return self.__lives
+
+    def __str__(self):
+        return f"\tStrength: {self.get_strength()}\n\tLives: {self.get_lives()}"
--- topic_05/Test_Alien.py
+++ topic_05/Test_Alien.py
@@ -1,33 +1,31 @@
 import unittest as ut
 from Alien_Class import *
 
+
 class AlienTestCase(ut.TestCase):
-  def setUp(self):
-    self.alien = Alien(8,10, "Martian")
-    print("Setup")
+    def setUp(self):
+        self.alien = Alien(8, 10, "Martian")
+        print("Setup")
 
+    def test_lives(self):
+        lives = self.alien.get_lives()
+        self.assertEqual(lives, 10)
 
-  def test_lives(self):
-    lives = self.alien.get_lives()
-    self.assertEqual(lives, 10)
-    #self.assertTrue(lives == 10)
+    def test_strength(self):
+        strength = self.alien.get_strength()
+        self.assertEqual(strength, 8)
 
+    def test_alien_type(self):
+        alien_type = self.alien.get_alien_type()
+        self.assertIn(alien_type, "Martian")
 
-  def test_strength(self):
-    strength = self.alien.get_strength()
-    self.assertEqual(strength, 8)
+    def test_alien_type_fail(self):
+        alien_type = self.alien.get_alien_type()
+        self.assertNotIn(alien_type, "martian")
 
-  def test_alien_type(self):
-    alien_type = self.alien.get_alien_type()
-    self.assertIn(alien_type, "Martian")
+    def tearDown(self):
+        del self.alien
+        print("Tear Down")
 
-  def test_alien_type_fail(self):
-    alien_type = self.alien.get_alien_type()
-    self.assertNotIn(alien_type, "martian")
-
-  def tearDown(self):
-    del self.alien
-    print("Tear Down")
-
-  if __name__ == '__main__':
-    ut.main()
+    if __name__ == '__main__':
+        ut.main()
